03e89ac002633ddd40d8a1319faf20feda7badc1
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-06-14 17:40:49 -0400

    Project Setup

---
+++ Starter-Warehouse/WarehouseSystem/WarehouseSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
